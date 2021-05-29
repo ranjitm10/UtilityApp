36e6234f62d1a1447cc6a3260da909bc535813d4
RM <>
2021-05-29 11:22:42 +0000

    add new features

--- index.js
+++ index.js
@@ -1,131 +1,164 @@
 function encodeUrl(){
 const input = readLineSync.question('Enter Url\n');
-console.log(`You have selected: ${input}`);
-console.log(encodeURIComponent(input));
-  }
+console.log(chalk.black.bgWhite(`Your Input: ${input}`));
+console.log(chalk.red.bgGreen(`Your Output: ${encodeURIComponent(input)}`));
+}
+
 
 function decodeUrl(){
 const input = readLineSync.question('Enter Url\n');
-console.log(`You have selected: ${input}`);
-console.log(decodeURIComponent(input));
-  }
-
-function decodeUrl(){
-const input = readLineSync.question('Enter Url\n');
-console.log(`You have selected: ${input}`);
-console.log((input));
-  }
+console.log(chalk.black.bgWhite(`Your Input: ${input}`));
+console.log(chalk.red.bgGreen(`Your Output${decodeURIComponent(input)}`));
+}
 
 
 function binaryToDecimal(){
-const input = readLineSync.question('Enter your number for converstion: ');
-console.log(`Converted: ${parseInt(input, 2)}`);
-  }
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
+console.log(chalk.red.bgGreen(`Converted: ${parseInt(input, 2)}`));
+}
+
+
 function binaryToHex(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const decimal = parseInt(input, 2);
-console.log(`Converted: ${decimal.toString(16)}`);
-  }
+console.log(chalk.red.bgGreen(`Converted: ${decimal.toString(16)}`));
+}
+
+
 function binaryToOctal(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const decimal = parseInt(input, 2);
-console.log(`Converted: ${decimal.toString(8)}`);
-  }
+console.log(chalk.red.bgGreen(`Converted: ${decimal.toString(8)}`));
+}
+
+
 function decimalToBinary(){
-const input = parseInt(readLineSync.question('Enter your number for converstion: '));
-console.log(`Converted: ${input.toString(2)}`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: ')));
+console.log(chalk.red.bgGreen(`Converted: ${input.toString(2)}`));
+}
+
+
 function decimalToHex(){
-const input = parseInt(readLineSync.question('Enter your number for converstion: '));
-console.log(`Converted: ${input.toString(16)}`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: ')));
+console.log(chalk.red.bgGreen(`Converted: ${input.toString(16)}`));
+}
+
+
+
 function decimalToOctal(){
-const input = parseInt(readLineSync.question('Enter your number for converstion: '));
-console.log(`Converted: ${input.toString(8)}`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: ')));
+console.log(chalk.red.bgGreen(`Converted: ${input.toString(8)}`));
+}
+
+
+
 function hexToBinary(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const hex = parseInt(input, 16);
-console.log(`${hex.toString(2)}`);
-  }
+console.log(chalk.red.bgGreen(`${hex.toString(2)}`));
+}
+
+
 function hexToDecimal(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const hex = parseInt(input, 16);
-console.log(`${hex.toString(10)}`);
-  }
+console.log(chalk.red.bgGreen(`${hex.toString(10)}`));
+}
+
+
 function hexToOctal(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const hex = parseInt(input, 16);
-console.log(`${hex.toString(8)}`);
-  }
+console.log(chalk.red.bgGreen(`${hex.toString(8)}`));
+}
+
+
+
 function octaltoBinary(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const octal = parseInt(input, 8);
-console.log(`${octal.toString(2)}`);
-  }
+console.log(chalk.red.bgGreen(`${octal.toString(2)}`));
+}
+
+
 function octaltoDecimal(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const octal = parseInt(input, 8);
-console.log(`${octal.toString(10)}`);
-  }
+console.log(chalk.red.bgGreen(`${octal.toString(10)}`));
+}
+
 
 
 
 function octaltoHex(){
-const input = readLineSync.question('Enter your number for converstion: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your number for converstion: '));
 const octal = parseInt(input, 8);
-console.log(`${octal.toString(16)}`);
-  }
+console.log(chalk.red.bgGreen(`${octal.toString(16)}`));
+}
+
+
 
 function md5(){
-const input = readLineSync.question('Enter your string: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your string: '));
 const algo = 'md5';
-console.log(`md5Hash("${input}")-->`,crypto.createHash(algo).update(input).digest('hex'));
-  }
+console.log(chalk.red.bgGreen(`md5Hash("${input}")-->`,crypto.createHash(algo).update(input).digest('hex')));
+}
+
+
 function sha1(){
-const input = readLineSync.question('Enter your string: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your string: '));
 const algo = 'sha1';
-console.log(`sha1("${input}")-->`,crypto.createHash(algo).update(input).digest('hex'));
+console.log(chalk.red.bgGreen(`sha1("${input}")-->`,crypto.createHash(algo).update(input).digest('hex')));
   }
+
+
 function sha256(){
-const input = readLineSync.question('Enter your string: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your string: '));
 const algo = 'sha256';
-console.log(`sha256("${input}")-->`,crypto.createHash(algo).update(input).digest('hex'));
-  }
+console.log(chalk.red.bgGreen(`sha256("${input}")-->`,crypto.createHash(algo).update(input).digest('hex')));
+}
+
 function sha512(){
-const input = readLineSync.question('Enter your string: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your string: '));
 const algo = 'sha512';
-console.log(`sha512("${input}")-->`,crypto.createHash(algo).update(input).digest('hex'));
-  }
+console.log(chalk.red.bgGreen(`sha512("${input}")-->`,crypto.createHash(algo).update(input).digest('hex')));
+}
+
 
 
 
 function toHumanDate(){
-const input = readLineSync.question('Enter your Epoch: ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter your Epoch: '));
 let myDate = new Date(input * 1000);
-console.log(`toHumanDate(${input})--> {year: ${myDate.getFullYear()}, month: ${myDate.getMonth()}, date: ${myDate.getDate()}, hours: ${myDate.getHours()}, minutes: ${myDate.getMinutes()}, seconds: ${myDate.getSeconds()}}`);
-  }
+console.log(chalk.red.bgGreen(`toHumanDate(${input})--> {year: ${myDate.getFullYear()}, month: ${myDate.getMonth()}, date: ${myDate.getDate()}, hours: ${myDate.getHours()}, minutes: ${myDate.getMinutes()}, seconds: ${myDate.getSeconds()}}`));
+}
+
+
 
 function toEpoch(){
-const year = readLineSync.question('Enter year: ');
-const month = readLineSync.question('Enter month: ');
-const date = readLineSync.question('Enter date: ');
-const hours = readLineSync.question('Enter hours: ');
-const minutes = readLineSync.question('Enter minutes: ');
-const seconds = readLineSync.question('Enter seconds: ');
+const year = readLineSync.question(chalk.black.bgWhite('Enter year: '));
+const month = readLineSync.question(chalk.black.bgWhite('Enter month: '));
+const date = readLineSync.question(chalk.black.bgWhite('Enter date: '));
+const hours = readLineSync.question(chalk.black.bgWhite('Enter hours: '));
+const minutes = readLineSync.question(chalk.black.bgWhite('Enter minutes: '));
+const seconds = readLineSync.question(chalk.black.bgWhite('Enter seconds: '));
 let myDate = new Date(year, month, date, hours, minutes, seconds);
-console.log(`toEpoch(${year}, ${month}, ${date}, ${hours}, ${minutes}, ${seconds},)--> ${myDate.getTime()/1000}`);
-  }
+console.log(chalk.red.bgGreen(`toEpoch(${year}, ${month}, ${date}, ${hours}, ${minutes}, ${seconds},)--> ${myDate.getTime()/1000}`));
+}
+
+
 
 function rgbToHex() {
-const redValue = parseInt(readLineSync.question('Enter Red value: '));
-const greenValue = parseInt(readLineSync.question('Enter Green value: '));
-const blueValue = parseInt(readLineSync.question('Enter Blue value: '));
-console.log(`RGB to Hex --> #${redValue.toString(16)}${greenValue.toString(16)}${blueValue.toString(16)}`);
-  }
+const redValue = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Red value: ')));
+const greenValue = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Green value: ')));
+const blueValue = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Blue value: ')));
+console.log(chalk.red.bgGreen(`RGB to Hex --> #${redValue.toString(16)}${greenValue.toString(16)}${blueValue.toString(16)}`));
+}
+
+
 
 function hexToRgb() {
-const input = readLineSync.question('Enter 6-Digit alpha numeric Hex Value(excluding "#"): ');
+const input = readLineSync.question(chalk.black.bgWhite('Enter 6-Digit alpha numeric Hex Value(excluding "#"): '));
 if (input.lenght != 6) {
   console.log("Invalid Value try again")
   }
@@ -136,35 +169,50 @@ const blueValue = `${input[2]}${input[4]}`;
 const blueHex = parseInt(blueValue,16);
 const greenValue = `${input[5]}${input[6]}`;
 const greenHex = parseInt(greenValue,16);
-console.log(`Red: ${redHex.toString(10)}, Blue: ${blueHex.toString(10)}, Green: ${greenHex.toString(10)}`);
+console.log(chalk.red.bgGreen(`Red: ${redHex.toString(10)}, Blue: ${blueHex.toString(10)}, Green: ${greenHex.toString(10)}`));
 }
-  }
+}
+
+
+
 function celsiusToKelvin(){
-const input = parseInt(readLineSync.question('Enter Value: '));
-console.log(`${input} Celsius = ${input + 273.15} Kelvin`);
-  }
+const input = parseInt(chalk.black.bgWhite(readLineSync.question('Enter Value: ')));
+console.log(chalk.red.bgGreen(`${input} Celsius = ${input + 273.15} Kelvin`));
+}
+
+
 
 function celsiusToFahrenheit(){
-const input = parseInt(readLineSync.question('Enter Value: '));
-console.log(`${input} Celsius = ${(input * 1.8) + 32} Fahrenheit`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Value: ')));
+console.log(chalk.red.bgGreen(`${input} Celsius = ${(input * 1.8) + 32} Fahrenheit`));
+}
+
+
 function kelvinToCelsius(){
-const input = parseInt(readLineSync.question('Enter Value: '));
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Value: ')));
 const kelvinValue = input - 273.15;
-console.log(`${input} Kelvin = ${kelvinValue.toFixed(2)} Celsius`);
-  }
+console.log(chalk.red.bgGreen(`${input} Kelvin = ${kelvinValue.toFixed(2)} Celsius`));
+}
+
+
 function kelvinToFahrenheit(){
-const input = parseInt(readLineSync.question('Enter Value: '));
-console.log(`${input} Kelvin = ${((input*1.8)-459.67).toFixed(2)} Fahrenheit`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Value: ')));
+console.log(chalk.red.bgGreen(`${input} Kelvin = ${((input*1.8)-459.67).toFixed(2)} Fahrenheit`));
+}
+
+
 function fahrenheitToCelsius(){
-const input = parseInt(readLineSync.question('Enter Value: '));
-console.log(`${input} Fahrenheit = ${((input - 32)*5/9).toFixed(2)} Celsius`);
-  }
+const input = parseInt(readLineSync.question(chalk.black.bgWhite('Enter Value: ')));
+console.log(chalk.red.bgGreen(`${input} Fahrenheit = ${((input - 32)*5/9).toFixed(2)} Celsius`));
+}
+
+
 function fahrenheitToKelvin(){
-const input = parseInt(readLineSync.question('Enter Value: '));
-console.log(`${input} Fahrenheit = ${(((input - 32)*5/9)+273.15).toFixed(2)} Kelvin`);
-  }
+const input = parseInt(readLrednc.queGreebgGreen(chalk.black.bgWhite('Enter Value: ')));
+console.log(chalk.red.bgGreen(`${input} Fahrenheit = ${(((input - 32)*5/9)+273.15).toFixed(2)} Kelvin`));
+}
+
+
 const chalk = require('chalk');
 const crypto = require('crypto');
 const readLineSync = require('readline-sync');
